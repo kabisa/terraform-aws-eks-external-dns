6c79850108feb9ff45407f487bb615d1dda3c2e1
Paul van Lierop <paul.van.lierop@kabisa.nl>
2022-04-12 12:11:53 +0200

    modified to use of templatefile()

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-data "template_file" "external-dns" {
-  template = file("${path.module}/yamls/external-dns-values.yaml")
-  vars     = {
-    region        = var.region
-    aws_role_arn  = aws_iam_role.external-dns-iam-role.arn
-    vpc_id        = var.vpc_id
-  }
-}
-
 resource "helm_release" "aws-load-balancer-controller" {
   name       = "external-dns"
   namespace  = "kube-system"
@@ -16,7 +7,14 @@ resource "helm_release" "aws-load-balancer-controller" {
   # appVersion: 0.7.6
 
   values = [
-    data.template_file.external-dns.rendered
+    templatefile(
+      "${path.module}/yamls/external-dns-values.yaml",
+      {
+        region       = var.region
+        aws_role_arn = aws_iam_role.external-dns-iam-role.arn
+        vpc_id       = var.vpc_id
+      }
+    )
   ]
 }
 
