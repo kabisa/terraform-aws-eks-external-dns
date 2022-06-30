e7e130ed44e3f94734890731f9e05bca36b161c8
Glaaj <83582518+Glaaj@users.noreply.github.com>
2022-06-30 10:40:35 +0200

    Adds descriptions to variables
    This is to prepare this module for terraform-docs

--- variables.tf
+++ variables.tf
@@ -1,13 +1,19 @@
 variable "region" {
   type = string
+  description = "The AWS region."
 }
 
 variable "oidc_host_path" {
   type = string
+  description = "The host path of the OIDC provider."
 }
 
-variable "vpc_id" {}
+variable "vpc_id" {
+  type = string
+  description = "The VPC ID."
+}
 
 variable "account_id" {
   type = string
+  description = "The AWS account ID."
 }
